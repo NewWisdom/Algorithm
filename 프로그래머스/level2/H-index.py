d178b691e30bfbb22dfc6498d1d46e641c380010
NewWisdom <wlgp2500@gmail.com>
2020-10-25 17:46:15 +0900

    H-index

---
+++ 프로그래머스/level2/H-index.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    answer = 0
+    citations.sort()
+    for i in range(len(citations)):
+        if citations[i] >= len(citations) - i:
+            return (len(citations)-i)
+
+    return 0
+
+print(solution([3, 0, 6, 1, 5]))
