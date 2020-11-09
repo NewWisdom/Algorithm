cf65ae06603deab12a6c46ccbedf9d8fc675b438
NewWisdom <wlgp2500@gmail.com>
2020-11-09 23:10:22 +0900

    JadenCase 문자열 만들기

---
+++ 프로그래머스/level2/JadenCase 문자열 만들기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = ''
+    for idx, i in enumerate(s): 
+        if (idx == 0): answer = i.upper()
+        elif s[idx-1] == ' ': answer = ''.join([answer, i.upper()])
+        else: answer = ''.join([answer, i.lower()])
+    return answer
+print(solution("3people unFollowed me"))
+
