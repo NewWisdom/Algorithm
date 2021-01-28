aa0cc7444838187a6ec33c934d6f64fdda509945
NewWisdom <wlgp2500@gmail.com>
2021-01-29 00:06:40 +0900

    가장 긴 펠린드롬

--- 프로그래머스/level3/가장 긴 펠린드롬.py
+++ 프로그래머스/level3/가장 긴 펠린드롬.py
@@ -18,20 +18,20 @@ abacde	3
 
 입출력 예 #2
 2번째자리 'b'를 기준으로 aba가 팰린드롬이 되므로 3을 return합니다.
-"""
-def check_palin(s):
-    print(s[::-1])
-    return s == s[::-1] # 역순
-    
-def solution(s):
-    answer = 0
-    length = len(s)
-    max_v = -1
-    for i in range(length):
-        for j in range(i, length):
-            if check_palin(s[i:j]):
-                if max_v < len(s[i:j]):
-                    max_v = len(s[i:j])
-    return max_v
+""" 
+def solution(s: str):
+    def expand(left: int, right: int) -> str:
+        # 현재 left, right index가 같은 문자일 경우 윈도우 크기를 좌우로 1씩 넓힌다.
+        while left >= 0 and right <= len(s) and s[left] == s[right-1]:
+            left -= 1
+            right += 1
+        return s[left+1:right-1]
+    # 연산이 필요없는 경우
+    if len(s) < 2 or s == s[::-1]:
+        return len(s)
+    result = ""
+    for i in range(len(s)-1):
+        result = max(result, expand(i, i+1), expand(i,i+2), key = len)
+    return result
   
 print(solution("abcdcba"))
